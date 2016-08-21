b54117d812ca3867e303920b172476369ad03d5c
hhatto <hhatto.jp@gmail.com>
2016-08-21 23:14:57 +0900

    rustfmt

--- src/woothee.rs
+++ src/woothee.rs
@@ -12,67 +12,66 @@ use woothee::{is_crawler, parser};
 
 #[repr(C)]
 pub enum VclEvent {
-	Load = 0,
-	Warm,
-	Use,
-	Cold,
-	Discard,
+    Load = 0,
+    Warm,
+    Use,
+    Cold,
+    Discard,
 }
 
-pub struct vmod_priv {                                                                                                                                                                                                 
-	prv	: *const Mutex<HashMap<String, c_int>>,
-	len	: c_int,
-	free	: *const c_void
+pub struct vmod_priv {
+    prv: *const Mutex<HashMap<String, c_int>>,
+    len: c_int,
+    free: *const c_void,
 }
 
 #[no_mangle]
-pub extern fn init_function(_	: *const c_void,
-			    prv	: &mut vmod_priv,
-			    ev	: VclEvent ) -> c_int{
-	match ev {
-		VclEvent::Warm => {
-			let hash = Mutex::new(HashMap::new());
-			prv.prv = Box::into_raw( Box::new(hash));
-		},
-		VclEvent::Cold => {
-			unsafe { Box::from_raw(prv);}
-		},
-		_ => ()
-	}
-	0
+pub extern "C" fn init_function(_: *const c_void, prv: &mut vmod_priv, ev: VclEvent) -> c_int {
+    match ev {
+        VclEvent::Warm => {
+            let hash = Mutex::new(HashMap::new());
+            prv.prv = Box::into_raw(Box::new(hash));
+        }
+        VclEvent::Cold => unsafe {
+            Box::from_raw(prv);
+        },
+        _ => (),
+    }
+    0
 }
 
 fn conv(input: *const c_char) -> String {
-	unsafe {
-		CString::new(String::new() +
-			     CStr::from_ptr(input).to_str().unwrap())
-	}.unwrap()
-	.into_string()
-	.unwrap()
+    unsafe { CString::new(String::new() + CStr::from_ptr(input).to_str().unwrap()) }
+        .unwrap()
+        .into_string()
+        .unwrap()
 }
 
 #[no_mangle]
-pub unsafe extern fn vmod_is_crawler(_: *const c_void, input: *const c_char) -> c_uint {
-	let ua = conv(input);
+pub unsafe extern "C" fn vmod_is_crawler(_: *const c_void, input: *const c_char) -> c_uint {
+    let ua = conv(input);
 
-	match is_crawler(ua.as_str()) {
-		true => { return 1 },
-		false => { return 0 },
-	}
+    match is_crawler(ua.as_str()) {
+        true => return 1,
+        false => return 0,
+    }
 }
 
 #[no_mangle]
-pub unsafe extern fn vmod_parse(_: *const c_void, prv: &vmod_priv, input: *const c_char) -> c_uint {
-	let ua = conv(input);
+pub unsafe extern "C" fn vmod_parse(_: *const c_void,
+                                    prv: &vmod_priv,
+                                    input: *const c_char)
+                                    -> c_uint {
+    let ua = conv(input);
     let p = parser::Parser::new();
 
-	match p.parse(ua.as_str()) {
-		Some(result) => {
+    match p.parse(ua.as_str()) {
+        Some(result) => {
             if result.category == "crawler" {
-                return 1
+                return 1;
             }
-            return 0
-        },
-		None => { return 0 }
-	}
+            return 0;
+        }
+        None => return 0,
+    }
 }

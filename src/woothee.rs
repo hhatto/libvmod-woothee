cc28044564beb3377c59bef83150a417dfac06ce
Hideo Hattori <hattori-h@klab.com>
2016-08-24 15:01:31 +0900

    wip

--- src/woothee.rs
+++ src/woothee.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 extern crate woothee;
-use libc::{c_int, c_uint, c_char, c_void};
+use libc::{c_int, c_uint, c_char, c_double, c_void, snprintf};
 
 use std::ffi::CStr;
 use std::ffi::CString;
@@ -9,6 +9,11 @@ use std::boxed::Box;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use woothee::{is_crawler, parser};
+
+extern "C" {
+    fn WS_Reserve(w: *const ws, b: c_uint) -> c_uint;
+    fn WS_Release(w: *const ws, b: c_uint);
+}
 
 #[repr(C)]
 pub enum VclEvent {
@@ -23,6 +28,40 @@ pub struct vmod_priv {
     prv: *const Mutex<HashMap<String, c_int>>,
     len: c_int,
     free: *const c_void,
+}
+
+pub struct vrt_ctx {
+    magic: c_uint,
+
+    method: c_uint,
+    handling: *const c_uint,
+
+    msg: *const c_void,
+    vsl: *const c_void,
+    vcl: *const c_void,
+    ws: *const ws,
+
+    req: *const c_void,
+    http_req: *const c_void,
+    http_req_top: *const c_void,
+    http_resp: *const c_void,
+
+    bo: *const c_void,
+    http_bereq: *const c_void,
+    http_beresp: *const c_void,
+
+    now: c_double,
+
+    specific: *const c_void,
+}
+
+struct ws {
+  magic: c_uint,
+  id: [c_char; 4],
+  s: *mut c_char,
+  f: *mut c_char,
+  r: *mut c_char,
+  e: *mut c_char,
 }
 
 #[no_mangle]
@@ -54,6 +93,27 @@ pub unsafe extern "C" fn vmod_is_crawler(_: *const c_void, input: *const c_char)
     match is_crawler(ua.as_str()) {
         true => return 1,
         false => return 0,
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vmod_parse_get_name(ctx: *const vrt_ctx, input: *const c_char) -> *const c_char {
+    let ua = conv(input);
+    let uaparser = parser::Parser::new();
+
+    let u = WS_Reserve((*ctx).ws, 0);
+    let p: *mut c_char = (*(*ctx).ws).f;
+    match uaparser.parse(ua.as_str()) {
+        Some(result) => {
+            let l = result.name.len() + 1;
+            let v = snprintf(p, l, CString::new(result.name).unwrap().as_ptr());
+            WS_Release((*ctx).ws, (v+1) as u32);
+            p
+        }
+        None => {
+            WS_Release((*ctx).ws, 0);
+            0 as (*const c_char)
+        }
     }
 }
 
